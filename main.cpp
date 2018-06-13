0d156c6e4effb94b209bdf55b13cab50b5f00f01
Andrej Suvorau <suvri4@gmail.com>
2018-06-13 22:12:07 +0300

    Got rid of using in global scope

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 
-using namespace std;
-
 int main(int argc, char *argv[]) {
-    cout << "Hello, world!" << endl;
+    std::cout << "Hello, world!" << std::endl;
     return 0;
 }
